7d3334dabd43b0f0be56445cf9b1613f481f106e
Piraya En-on - 6609650087 <piraya.en-@dome.tu.ac.th>
2026-04-27 02:30:48 +0700

    feat(category): register category routes

--- handlers/category.go
+++ handlers/category.go
@@ -11,3 +11,7 @@ var categories []models.Category
 func GetCategories(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(categories)
 }
+
+func CategoryHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -17,6 +17,11 @@ func main() {
 	http.HandleFunc("/api/notifications", handlers.GetNotifications)
 	http.HandleFunc("/api/notifications/", handlers.NotificationHandler)
 
+	//Category  routes
+	http.HandleFunc("/api/categories", handlers.GetCategories)
+	http.HandleFunc("/api/categories/", handlers.CategoryHandler)
+
 	log.Println("Server running on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
+
 }
